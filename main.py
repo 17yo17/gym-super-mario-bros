030bb496ef26a262c4c069e1f52f3b003d78d6e4
17yoyo17 <>
2024-11-24 03:12:44 +0000

    Added argument parser and multiprocessing for A3C

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # Setup
+# argument parser
 import argparse
+# time watch
 import time
+
 import torch
 import torch.multiprocessing as _mp
 #from test import test
 
 # import the game
 import gym_super_mario_bros
-# import the joypad wrapper
-from nes_py.wrappers import JoypadSpace
-# import the SIMPLIFIED controls
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT ## [['NOOP'], ['right'], ['right', 'A'], ['right', 'B'], ['right', 'A', 'B'], ['A'], ['left']]
+from agent import Agent
+from env import MultipleEnvironments
 # import Matplotlib to show the impact of frame stacking
 from matplotlib import pyplot as plt
 import numpy as np
@@ -23,6 +24,7 @@ def parse():
     parser.add_argument('--world', default=1, help='Indicate World: [1,2,3,4,5,6,7,8]')
     parser.add_argument('--stage', default=1, help='Indicate Stage: [1,2,3,4]')
     parser.add_argument('--action_type', default='simple', help='Action Space: right/simple/complex')
+    parser.add_argument('--num_processes', type=int, default=8, help='Number of environments to process simultaneously')
     parser.add_argument('--train', action='store_true', help='Train a Model')
     parser.add_argument('--test', action='store_true', help='Test a Model')
     parser.add_argument('--record', action='store_true', help='Record video')
@@ -35,14 +37,14 @@ def parse():
     return args
 
 def run(args, record_video=False):
-    start_time = time.time()
+    start_time = time.time() 
+     
+    agent = Agent(args)
+    
     if args.train:
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed(123)
-        else:
-            torch.manual_seed(123)
-        mp = _mp.get_context("spawn")
-
+        agent.train()
+    if args.test:
+        agent.test()
 
 if __name__ == '__main__':
     args = parse()
